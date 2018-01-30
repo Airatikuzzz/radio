febbb07e6a8bdbc532ae23c15a52df332143336a
Airatikuzzz <m.airat1997@gmail.com>
2018-01-30 17:33:51 +0300

    fires

--- app/src/main/java/com/airatikuzzz/radio/stations/StationsLab.java
+++ app/src/main/java/com/airatikuzzz/radio/stations/StationsLab.java
@@ -3,6 +3,8 @@ package com.airatikuzzz.radio.stations;
 import android.content.Context;
 
 import com.airatikuzzz.radio.R;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.google.gson.reflect.TypeToken;
@@ -13,6 +15,7 @@ import java.io.Reader;
 import java.lang.reflect.Type;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * Created by maira on 25.01.2018.
@@ -37,5 +40,16 @@ public class StationsLab {
         Reader reader = new InputStreamReader(context.getResources().openRawResource(R.raw.shoutcasts));
         mRadioStations = gson.fromJson(reader, type);
         expDetails.put("Станции", mRadioStations);
+        toFireStore();
+
+    }
+
+    public void toFireStore(){
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        CollectionReference stations = db.collection("stations");
+
+        for(RadioStation station : mRadioStations){
+            stations.add(station);
+        }
     }
 }
